68ad22ff2d01c6695bfa0514b36fc6f31933c827
samanta-scavassa <samanta.camargoterra@gmail.com>
2024-02-29 10:25:56 +0100

    Add Login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import SignupPage from "./pages/SignupPage";
 import ErrorPage from "./pages/ErrorPage";
+import LoginPage from "./pages/LoginPage";
+import IsAnon from "./components/IsAnon";
 
 function App() {
   return (
     <Routes>
       <Route path="/hungry-hub" element={<HomePage />} />
       <Route path="/hungry-hub/signup" element={<SignupPage />} />
+      <Route path="/hungry-hub/login" element={<LoginPage />} />
       <Route path="*" element={<ErrorPage />} />
     </Routes>
   );
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,90 @@
+import { Button, TextField } from "@mui/material";
+import authService from "../services/auth.service";
+import { useNavigate } from "react-router";
+import { useContext, useState } from "react";
+// import { AuthContext } from "../context/auth.context";
+import { Stack } from "@mui/system";
+import "./LoginPage.css";
+
+export default function LoginPage(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+//   const { storeToken, authenticateUser } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handlePassword = (e) => setPassword(e.target.value);
+
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    const requestBody = { email, password };
+
+    authService
+      .login(requestBody)
+      .then((response) => {
+        // storeToken(response.data.authToken);
+        // authenticateUser();
+        navigate("/hungry-hub");
+      })
+      .catch((error) => {
+        const errorDescription = error.response.data.message;
+        console.error(errorDescription);
+        navigate("/*");
+      });
+  };
+
+  return (
+    <div>
+      <div className="LoginBox">
+        <h1>Login</h1>
+        <p>Sign in to continue</p>
+
+        <form className="LoginForm" onSubmit={handleLoginSubmit}>
+          <Stack spacing={2} direction="column" sx={{ margin: 4 }}>
+            <label>E-MAIL</label>
+            <TextField
+              id="outlined-required"
+              label="E-mail"
+              name="email"
+              type="email"
+              value={email}
+              onChange={handleEmail}
+              required
+            />
+
+            <label>PASSWORD</label>
+            <TextField
+              id="outlined-required"
+              label="Password"
+              name="password"
+              type="password"
+              value={password}
+              onChange={handlePassword}
+              required
+            />
+          </Stack>
+          <div className="LoginButtons">
+            <Button
+              sx={{ backgroundColor: "white", color: "#EF233C" }}
+              variant="outlined"
+              color="error"
+              onClick={() => navigate("/hungry-hub")}
+            >
+              BACK
+            </Button>
+            <Button
+              sx={{ backgroundColor: "#EF233C", color: "white" }}
+              variant="contained"
+              color="error"
+              type="submit"
+            >
+              LOGIN
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
