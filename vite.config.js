bf7dd2fe4edd2bc63543e3ff404e6811b9fcd528
samanta-scavassa <samanta.camargoterra@gmail.com>
2024-03-10 20:01:19 +0100

    adjust deploy env viariable

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'import.meta.env.SERVER_URL': JSON.stringify(import.meta.env.SERVER_URL),
+  },
   plugins: [react()],
   base: "/hungry-hub/",
 });
